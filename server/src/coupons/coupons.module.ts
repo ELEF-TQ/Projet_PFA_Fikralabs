3ffd1fd49975e2dc0443bffc6ac92cc0f57bbec0
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2024-04-19 19:18:35 +0100

    update client solde

--- server/src/coupons/coupons.module.ts
+++ server/src/coupons/coupons.module.ts
@@ -4,13 +4,12 @@ import { CouponsController } from './coupons.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Coupon, CouponSchema } from './Schemas/coupon.schema';
 import { ClientsModule } from 'src/clients/clients.module';
+import { Client, ClientSchema } from 'src/clients/schemas/client.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([
-    {
-      name: Coupon.name,
-      schema: CouponSchema
-    }
+    {name: Coupon.name,schema: CouponSchema},
+    {name: Client.name,schema: ClientSchema}
   ]),
     ClientsModule],
   controllers: [CouponsController],
