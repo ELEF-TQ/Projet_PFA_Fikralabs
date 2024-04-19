cae959a45fa2d5e1d3cbf19f264508b29225f9bb
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2024-04-19 19:33:50 +0100

    bug in coupon delete

--- client/src/pages/Admin/GestCoupons/Coupon.tsx
+++ client/src/pages/Admin/GestCoupons/Coupon.tsx
@@ -209,7 +209,9 @@ const Coupon : React.FC = () => {
                             </svg>
                           </button>
                           
-                          <button onClick={() =>{setSelectedId(coupon._id) , setIsDeleteModalOpen(true)}}  type="button" data-modal-target="delete-modal" data-modal-toggle="delete-modal" className="flex items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+                          <button 
+                          disabled={new Date(coupon.dateExpiration) > new Date()} 
+                          onClick={() =>{setSelectedId(coupon._id) , setIsDeleteModalOpen(true)}}  type="button" data-modal-target="delete-modal" data-modal-toggle="delete-modal" className="flex items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4  -ml-0.5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                               <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
                             </svg>
