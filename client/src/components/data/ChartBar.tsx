db3b288a02265ab73327bbad63bbcff64d4b9073
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2024-04-20 19:02:11 +0100

    statistics

--- client/src/components/data/ChartBar.tsx
+++ client/src/components/data/ChartBar.tsx
@@ -18,13 +18,33 @@ const ChartBar: React.FC<ChartProps> = ({ data }) => {
         type: "bar",
         data: data,
         options: {
+          maintainAspectRatio: false, // Ensure the chart fills its container
+          plugins: {
+            legend: {
+              display: false, // Hide the legend
+            },
+          },
           scales: {
+            x: {
+              grid: {
+                display: false, // Hide the x-axis grid lines
+              },
+            },
             y: {
               beginAtZero: true,
               grid: {
                 display: true,
                 color: "rgba(0, 0, 0, 0.1)",
               },
+              ticks: {
+                color: "rgba(0, 0, 0, 0.8)", // Set the color of the tick marks on the y-axis
+              },
+            },
+          },
+          elements: {
+            bar: {
+              borderWidth: 2,
+              borderRadius: 2,
             },
           },
         },
@@ -37,7 +57,30 @@ const ChartBar: React.FC<ChartProps> = ({ data }) => {
     };
   }, [data]);
 
-  return <canvas ref={chartRef} />;
+  // Generate an array of random colors
+  const generateRandomColors = (numColors: number) => {
+    const colors = [];
+    for (let i = 0; i < numColors; i++) {
+      const color = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)}, 0.6)`;
+      colors.push(color);
+    }
+    return colors;
+  };
+
+  useEffect(() => {
+    if (chartInstance && data.datasets) {
+      const numBars = data.datasets[0].data.length;
+      const randomColors = generateRandomColors(numBars);
+      chartInstance.data.datasets?.forEach((dataset: any, index: number) => {
+        dataset.backgroundColor = randomColors[index];
+      });
+      chartInstance.update();
+    }
+  }, [data]);
+
+  return <canvas ref={chartRef} style={{ maxWidth: "100%" }} />;
 };
 
 export default ChartBar;
--- client/src/pages/Admin/Panel.tsx
+++ client/src/pages/Admin/Panel.tsx
@@ -1,43 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { FaUser, FaMoneyCheckAlt, FaClipboardList, FaTags, FaTools } from "react-icons/fa";
+import { RiUser3Line, RiClipboardLine, RiCoupon2Line, RiToolsLine, RiStarLine } from 'react-icons/ri';
+import { LiaChargingStationSolid } from "react-icons/lia";
 import "./style.css";
 import ChartComponent from "../../components/data/ChartBar";
 import ReviewsChart from "../../components/data/ChartPolar";
+import { chartData, reviewsData } from '../../mock/statistics'
+import {  axiosNoAuth } from "../../lib/AxiosBase";
 
 const Panel: React.FC = () => {
-  const [chartData, setChartData] = useState<any>(null);
-  const [reviewsData, setReviewsData] = useState<number[]>([]);
-
+ 
+  const [countData , setCountData] = useState<any>(null)
+ 
   useEffect(() => {
-    // Mock data for the chart
-    const data = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
-        {
-          label: "Sample Data",
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          borderColor: "rgba(255, 99, 132, 1)",
-          borderWidth: 1,
-          hoverBackgroundColor: "rgba(255, 99, 132, 0.4)",
-          hoverBorderColor: "rgba(255, 99, 132, 1)",
-          data: [65, 59, 80, 81, 56, 55, 40]
-        }
-      ]
-    };
-
-    setChartData(data);
-
-    // Mock data for reviews
-    const reviews = [10, 25, 30, 45, 60, 55];
-    setReviewsData(reviews);
+    axiosNoAuth.get('/statistics/countDocs')
+      .then(response => {
+        console.log(response)
+        setCountData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching count data:', error);
+      });
   }, []);
 
   const cards = [
-    { type: "Pompistes", color: "bg-green-200", icon: <FaUser className="w-8 h-8 text-green-500" /> },
-    { type: "Clients", color: "bg-yellow-200", icon: <FaMoneyCheckAlt className="w-8 h-8 text-yellow-500" /> },
-    { type: "Conversion", color: "bg-red-200", icon: <FaClipboardList className="w-8 h-8 text-red-500" /> },
-    { type: "Coupon", color: "bg-purple-200", icon: <FaTags className="w-8 h-8 text-purple-500" /> },
-    { type: "Service", color: "bg-indigo-200", icon: <FaTools className="w-8 h-8 text-indigo-500" /> }
+    { type: "Pompistes", color: "bg-green-200", icon: <LiaChargingStationSolid className="w-8 h-8 text-green-600" />, count: countData?.pompistesCount },
+    { type: "Clients", color: "bg-yellow-200", icon: <RiUser3Line className="w-8 h-8 text-yellow-600" />, count: countData?.clientsCount },
+    { type: "Conversion", color: "bg-red-200", icon: <RiClipboardLine className="w-8 h-8 text-red-600" />, count: countData?.conversionsCount },
+    { type: "Coupon", color: "bg-purple-200", icon: <RiCoupon2Line className="w-8 h-8 text-purple-600" />, count: countData?.couponsCount },
+    { type: "Service", color: "bg-indigo-200", icon: <RiToolsLine className="w-8 h-8 text-indigo-600" />, count: countData?.servicesCount },
+    { type: "Reviews", color: "bg-pink-200", icon: <RiStarLine className="w-8 h-8 text-pink-600" />, count: countData?.reviewsCount }
   ];
 
   return (
@@ -48,7 +39,7 @@ const Panel: React.FC = () => {
           <div key={index} className="w-56 card">
             <div className={`${card.color} p-2 rounded-md flex items-center justify-between shadow-md relative transition duration-300 ease-in-out transform hover:scale-105`}>
               <div className="flex items-center justify-center h-12 w-12 rounded-full bg-white shadow-md">
-                <h1 className="font-bold text-xl text-gray-700">{index + 1}</h1>
+                <h1 className="font-bold text-xl text-gray-700">{card.count}</h1>
               </div>
               <div className="p-2">
                 <p className="text-sm font-semibold">{card.type}</p>
