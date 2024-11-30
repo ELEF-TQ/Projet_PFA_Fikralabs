c37288fe27dcbe4d52c34504d4b4ea1b5b3c0f70
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2024-11-30 21:16:53 +0100

    SonarQube

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import seedPermissions from 'migration/seed-data';
 import { Connection } from 'mongoose';
 import { getConnectionToken } from '@nestjs/mongoose';
@@ -11,14 +10,6 @@ async function bootstrap() {
   // const connection = app.get<Connection>(getConnectionToken());
   // await seedPermissions(connection); 
 
-  
-  const config = new DocumentBuilder()
-  .setTitle('Vivo Energy API')
-  .setDescription('API testing')
-  .setVersion('1.0')
-  .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.listen(3001);
 }
